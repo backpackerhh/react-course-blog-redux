01ed1ec3a5cd08a739b56c2d193a979bbe7fd512
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-23 09:09:51 +0100

    Allow to display info about each post

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,7 +8,24 @@ const PostList = props => {
     props.fetchPosts();
   }, []);
 
-  return <div>PostList</div>;
+  const renderList = () => {
+    return props.posts.map(post => {
+      return (
+        <div key={post.id} className="item">
+          <i className="large middle aligned icon user"></i>
+
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return <div className="ui relaxed divided list">{renderList()}</div>;
 };
 
 const mapStateToProps = state => state;
